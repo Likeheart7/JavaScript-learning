b0632e17ff43642ee2431c756a3529c274177e65
chenxing <1057791979@qq.com>
2024-05-13 16:16:14 +0800

    provide、inject跨多层组件通信

---
+++ vue/08-potential-problem-starting-up/src/components/KnowledgeElement.vue
@@ -0,0 +1,17 @@
+<template>
+	<li>
+		<h3>{{ topicName }}</h3>
+		<p>{{ description }}</p>
+		<button @click="selectTopic(id)">Learn More</button>
+	</li>
+</template>
+
+<script>
+export default {
+	// 接收祖先组件通过provide提供的数据
+	// 如果是data，约等于跨层级的props
+	// 如果是方法，约等于跨层级的emits
+	inject: ['selectTopic'],
+	props: ['id', 'topicName', 'description'],
+};
+</script>
